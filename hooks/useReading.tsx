8e94584ee105bff4f6d4f7b6e154e46e0315aa73
khoahuynh <khoahuynh@vietswiss.com>
2023-04-17 16:26:48 +0700

    fix

---
+++ hooks/useReading.tsx
@@ -0,0 +1,37 @@
+import {LegacyRef, useEffect} from "react";
+
+const useReading = (refReading: any) => {
+
+  useEffect(()=>{
+    console.log("document",document)
+    if(document && refReading && refReading?.current) {
+      // refReading.current.addEventListener("mouseup",handleMouseUp)
+      refReading.current.addEventListener("touchend",handleTouch)
+
+    }
+    return () => {
+      // refReading && refReading.current && refReading.current.removeEventListener("mouseup",handleMouseUp)
+      refReading && refReading.current && refReading.current.removeEventListener("touchend",handleTouch)
+
+    }
+  },[])
+
+  function handleTouch(e){
+    console.log("selection",window.getSelection().toString())
+    console.log("selection2",document.getSelection().toString())
+  }
+
+  const handleMouseUp = (e: MouseEvent) => {
+    const ele : HTMLDivElement = refReading.current
+    console.log("handleMouseUp")
+    if(document) {
+      const textSelect = document.getSelection().toString();
+      if(textSelect && textSelect !== '') {
+        alert(textSelect)
+      }
+    }
+  }
+
+  return {}
+}
+export default useReading
